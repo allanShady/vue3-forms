7697a2e2194e3af43541d8bba07c7b52e39e8369
allanShady <acamilo.ag@gmail.com>
2021-07-03 06:15:42 +0200

    feat: add base select atom

---
+++ src/components/atoms/BaseSelect.vue
@@ -0,0 +1,33 @@
+<template>
+    <label>{{label}}</label>
+        <select 
+            :value="modelValue" 
+            class="field" 
+            v-bind="{...$attrs, onChange: ($event) => { $emit('update:modelValue', $event.target.value) }}">
+          <option
+            v-for="option in options"
+            :value="option"
+            :key="option"
+            :selected="option === modelValue"
+          >{{ option }}</option>
+        </select>
+</template>
+
+<script>
+export default {
+    props: {    
+        label: {
+            type: String,
+            default: ''
+        },
+        modelValue: {
+            type: [String, Number],
+            default: ''
+        },
+        options: {
+            type: Array,
+            required: true
+        }
+    }
+}
+</script>
