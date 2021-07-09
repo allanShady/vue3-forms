823e2a208c04d65a822e12afca4ce9b733e2b436
allanShady <acamilo.ag@gmail.com>
2021-07-09 06:31:37 +0200

    refactor: set uniqueID for label and input

--- src/components/BaseInput.vue
+++ src/components/BaseInput.vue
@@ -1,16 +1,19 @@
 <template>
-    <label>{{label}}</label>
+    <label :for="uuid" v-if="label" >{{label}}</label>
         <input
             v-bind="$attrs"
             :placeholder="label"
             class="field"
             :value="modelValue"
             @input="$emit('update:modelValue', $event.target.value)"
+            :id="uuid"
         />
       
 </template>
 
 <script>
+import UniqueID from '../services/UniqueID'
+
 export default {
     props: {
         label: {
@@ -21,6 +24,12 @@ export default {
             type: [String, Number],
             default: ''
         }
+    },
+
+    setup () {
+        const uuid = UniqueID().getId()
+
+        return { uuid }
     }
 }
 </script>
